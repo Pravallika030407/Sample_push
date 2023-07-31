461f9defb5d167b01c5a74f1ec0a6ae6c9e62379
pravallika <pravallika1249@gmail.com>
2023-07-31 22:49:01 +0530

    1-2

--- check-appointment.php
+++ check-appointment.php
@@ -155,6 +155,7 @@ $cnt=$cnt+1;
         </main>
         <?php include_once('includes/footer.php');?>
         <!-- JAVASCRIPT FILES -->
+        <!-- -->
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.bundle.min.js"></script>
         <script src="js/owl.carousel.min.js"></script>
